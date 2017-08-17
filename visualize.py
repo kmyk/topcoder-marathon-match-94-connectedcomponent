8b7b274a28bae3d1f5a7a49692d466e70fd0bf29
Kimiyuki Onaka <kimiyuki95@gmail.com>
2017-08-18 08:34:27 +0900

    visualizer

--- visualize.py
+++ visualize.py
@@ -16,53 +16,64 @@ def read_matrix(path):
     return matrix
 
 def read_ps(fh):
+    ps = []
     for line in fh:
         if line.startswith('VISUALIZE: '):
             line = line.split()
             p = list(map(int, line[2 : ]))
             assert len(p) == int(line[1])
-            yield p
+            ps += [ { 'p': p } ]
+        elif line.startswith('MESSAGE: '):
+            message = line[len('MESSAGE: ') : ]
+            if 'message' in ps[-1]:
+                ps[-1]['message'] += ' / ' + message
+            else:
+                ps[-1]['message'] = message
+    return ps
 
-def flood_fill(h, w, y, x, callback):
+def flood_fill(h, w, y, x, callback, pred):
     dy = [ -1, 1, 0, 0 ]
     dx = [ 0, 0, 1, -1 ]
     used = [ False ] * (h * w)
-    stk = [ (y, x) ]
-    used[y * w + x] = True
+    stk = []
+    if pred(y, x):
+        used[y * w + x] = True
+        stk += [ (y, x) ]
     while stk:
         y, x = stk[-1]
         stk.pop()
-        if callback(y, x):
-            for i in range(4):
-                ny = y + dy[i]
-                nx = x + dx[i]
-                if 0 <= ny < h and 0 <= nx < w and not used[ny * w + nx]:
-                    used[ny * w + nx] = True
-                    stk += [ (ny, nx) ]
+        callback(y, x)
+        for i in range(4):
+            ny = y + dy[i]
+            nx = x + dx[i]
+            if 0 <= ny < h and 0 <= nx < w and not used[ny * w + nx] and pred(ny, nx):
+                used[ny * w + nx] = True
+                stk += [ (ny, nx) ]
 
 def calculate_score(s, matrix, p):
     used = [ False ] * (s ** 2)
     result = { 'score': -1 }
     for y in range(s):
         for x in range(s):
-            if not used[y * s + x]:
+            if not used[y * s + x] and matrix[p[y] * s + p[x]]:
                 size = 0
                 acc = 0
+                component = [ False ] * (s ** 2)
                 def callback(y, x):
-                    nonlocal used, size, acc
-                    value = matrix[p[y] * s + p[x]]
-                    if value == 0:
-                        return False
+                    nonlocal used, size, acc, component
                     used[y * s + x] = True
                     size += 1
-                    acc += value
-                    return True
-                flood_fill(s, s, y, x, callback)
+                    acc += matrix[p[y] * s + p[x]]
+                    component[y * s + x] = True
+                def pred(y, x):
+                    return not used[y * s + x] and matrix[p[y] * s + p[x]]
+                flood_fill(s, s, y, x, callback, pred)
                 score = int(acc * math.sqrt(size))
                 if result['score'] < score:
                     result['score'] = score
                     result['size'] = size
                     result['sum'] = acc
+                    result['used'] = component
                     result['y'] = y
                     result['x'] = x
     return result
@@ -77,30 +88,26 @@ def get_color(m, highlight):
         base = [0x111111, 0x110000][bool(highlight)]
         return (0xF - m * 0x1) * base
 
-def render_image(screen, s, matrix, p, result, scale):
-    font = pygame.font.SysFont(None, 12)
-    used = [ False ] * (s ** 2)
-    def callback(y, x):
-        nonlocal used
-        if matrix[p[y] * s + p[x]] == 0:
-            return False
-        used[y * s + x] = True
-        return True
-    flood_fill(s, s, result['y'], result['x'], callback)
+def render_image(screen, s, matrix, p, scale):
     screen.fill(0x000000)
     for y in range(s):
         for x in range(s):
             width = 1
             rect = pygame.Rect( x * scale, y * scale, scale, scale )
-            pygame.draw.rect(screen, [0xd3d3d3, 0x000000][used[y * s + x]], rect)  # draw the border
-            color = get_color(matrix[p[y] * s + p[x]], used[y * s + x])
+            pygame.draw.rect(screen, [0xd3d3d3, 0x000000][p['used'][y * s + x]], rect)  # draw the border
+            color = get_color(matrix[p['p'][y] * s + p['p'][x]], p['used'][y * s + x])
             rect = pygame.Rect( x * scale + width, y * scale + width, scale - 2 * width, scale - 2 * width )
             pygame.draw.rect(screen, color, rect)
 
 def update_screen(screen, s, matrix, ps, ix, scale):
-    result = calculate_score(s, matrix, ps[ix])
-    render_image(screen, s, matrix, ps[ix], result, scale)
-    pygame.display.set_caption('visualize %d / %d: score = %d' % (ix + 1, len(ps), result['score']))
+    p = ps[ix]
+    if 'score' not in p:
+        p.update(calculate_score(s, matrix, p['p']))
+    render_image(screen, s, matrix, p, scale)
+    caption = 'visualize %d of %d / score = %d' % (ix + 1, len(ps), p['score'])
+    if 'message' in p:
+        caption += '/ ' + p['message']
+    pygame.display.set_caption(caption)
     pygame.display.update()
 
 def visualize(s, matrix, ps):
@@ -121,6 +128,10 @@ def visualize(s, matrix, ps):
                 elif event.key == pygame.K_LEFT:
                     ix = (ix - 1) % len(ps)
                     update_screen(screen, s, matrix, ps, ix, scale)
+                if event.key == pygame.K_a:
+                    for p in ps:
+                        if 'score' not in p:
+                            p.update(calculate_score(s, matrix, p['p']))
         time.sleep(1 / 60)
     pygame.quit()
 
@@ -135,9 +146,9 @@ if __name__ == '__main__':
     s = int(math.sqrt(len(matrix)))
 
     if args.output is None:
-        ps = list(read_ps(sys.stdin))
+        ps = read_ps(sys.stdin)
     else:
         with open(args.output) as fh:
-            ps = list(read_ps(fh))
+            ps = read_ps(fh)
 
     visualize(s, matrix, ps)
