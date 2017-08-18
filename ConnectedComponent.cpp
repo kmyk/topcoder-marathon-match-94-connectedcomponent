57110f469a2052fa5fc3d8cc77ad91ba2bdef666
Kimiyuki Onaka <kimiyuki95@gmail.com>
2017-08-18 12:43:32 +0900

    最大連結成分の周囲のみ見る

--- ConnectedComponent.cpp
+++ ConnectedComponent.cpp
@@ -63,20 +63,28 @@ double calculate_score(vector<int> const & p, vector<char> const & matrix) {
     return result;
 }
 
-pair<double, double> calculate_evaluated_value(vector<int> const & p, vector<char> const & matrix) {
+struct permutation_info_t {
+    double score;
+    double evaluated;
+    int ly, lx, ry, rx;
+};
+permutation_info_t analyze_permutation(vector<int> const & p, vector<char> const & matrix, int ly, int lx, int ry, int rx) {
     int s = p.size();
     auto at = [&](int y, int x) { return matrix[p[y] * s + p[x]]; };
     auto is_on_field = [&](int y, int x) { return 0 <= y and y < s and 0 <= x and x < s; };
     vector<char> used(s * s);
-    int size = 0, acc = 0;
+    int size = -1, acc = -1;
     bool centered = false;
+    int nly = -1, nlx = -1, nry = -1, nrx = -1;
     function<void (int, int)> go = [&](int y, int x) {
         used[y * s + x] = true;
         size += 1;
         acc += at(y, x);
-        if (abs(y - s / 2) + abs(x - s / 2) <= 3) {
-            centered = true;
-        }
+        if (abs(y - s / 2) + abs(x - s / 2) <= 3) centered = true;
+        setmin(nly, y);
+        setmin(nlx, x);
+        setmax(nry, y + 1);
+        setmax(nry, x + 1);
         repeat (i, 4) {
             int ny = y + dy[i];
             int nx = x + dx[i];
@@ -85,22 +93,26 @@ pair<double, double> calculate_evaluated_value(vector<int> const & p, vector<cha
             }
         }
     };
-    double score = 0;
-    double evaluated = 0;
-    repeat (y, s) repeat (x, s) {
+    permutation_info_t info = {};
+    repeat_from (y, ly, ry) repeat_from (x, lx, rx) {
         if (not used[y * s + x] and at(y, x)) {
             size = acc = 0;
             centered = false;
+            nly = y; nlx = x; nry = y + 1; nrx = x + 1;
             go(y, x);
-            if (score < acc * sqrt(size)) {
-                score = acc * sqrt(size);
+            if (info.score < acc * sqrt(size)) {
+                info.score = acc * sqrt(size);
             }
-            if (centered and evaluated < acc * sqrt(size)) {
-                evaluated = acc * sqrt(size);
+            if (centered and info.evaluated < acc * sqrt(size)) {
+                info.evaluated = acc * sqrt(size);
+                info.ly = nly;
+                info.lx = nlx;
+                info.ry = nry;
+                info.rx = nrx;
             }
         }
     }
-    return { evaluated, score };
+    return info;
 }
 
 void visualize(vector<int> const & p) {
@@ -135,28 +147,29 @@ vector<int> ConnectedComponent::permute(vector<int> int_matrix) {
     // initialize
     vector<int> p(s);
     iota(whole(p), 0);
-visualize(p);
-cerr << "MESSAGE: s = " << s << endl;
-{
-    vector<int> cnt(s);
-    repeat (y, s) {
-        repeat (x, s) {
-            int m = matrix[p[y] * s + p[x]];
-            cnt[p[y]] += max<int>(0, m ? m + 4 : 0);
-            cnt[p[x]] += max<int>(0, m ? m + 4 : 0);
+// visualize(p);
+// cerr << "MESSAGE: s = " << s << endl;
+    {
+        vector<int> cnt(s);
+        repeat (y, s) {
+            repeat (x, s) {
+                int m = matrix[p[y] * s + p[x]];
+                cnt[p[y]] += max<int>(0, m ? m + 4 : 0);
+                cnt[p[x]] += max<int>(0, m ? m + 4 : 0);
+            }
         }
+        vector<int> t(s);
+        iota(whole(t), 0);
+        sort(whole(t), [&](int i, int j) { return cnt[i] > cnt[j]; });
+        p.clear();
+        for (int i = 0; i < s; i += 2) p.push_back(t[i]);
+        reverse(whole(p));
+        for (int i = 1; i < s; i += 2) p.push_back(t[i]);
+// visualize(p);
     }
-    vector<int> t(s);
-    iota(whole(t), 0);
-    sort(whole(t), [&](int i, int j) { return cnt[i] > cnt[j]; });
-    p.clear();
-    for (int i = 0; i < s; i += 2) p.push_back(t[i]);
-    reverse(whole(p));
-    for (int i = 1; i < s; i += 2) p.push_back(t[i]);
-visualize(p);
-}
-    double best_score, current_evaluated; tie(current_evaluated, best_score) = calculate_evaluated_value(p, matrix);
+    auto current = analyze_permutation(p, matrix, 0, 0, s, s);
     vector<int> result = p;
+    double best_score = current.score;
 /*
     repeat (iteration, 100) {
         shuffle(whole(p), gen);
@@ -178,8 +191,8 @@ cerr << "MESSAGE: score     = " << int(best_score) << endl;
         if (iteration % 10 == 0) {
             double clock_end = rdtsc();
             if (clock_end - clock_begin > 9.5) {
-cerr << "MESSAGE: iteration = " << iteration << endl;
-cerr << "MESSAGE: ratio = " << best_score / estimate_base_score(s, matrix) << endl;
+// cerr << "MESSAGE: iteration = " << iteration << endl;
+// cerr << "MESSAGE: ratio = " << best_score / estimate_base_score(s, matrix) << endl;
                 break;
             }
             temp = max(0.0, 10 - (clock_end - clock_begin)) / 10 * s * 10;
@@ -189,23 +202,28 @@ cerr << "MESSAGE: ratio = " << best_score / estimate_base_score(s, matrix) << en
             uniform_int_distribution<int>(max(s / 2, (s - 1) - x - 3), s - 1)(gen) :
             uniform_int_distribution<int>(0, min(s / 2 - 1, (s - 1) - x + 3))(gen);
         swap(p[x], p[y]);
-        double next_evaluated, next_score; tie(next_evaluated, next_score) = calculate_evaluated_value(p, matrix);
-        double delta = next_evaluated - current_evaluated;
-        if (current_evaluated < next_evaluated + eps or bernoulli_distribution(exp(delta / temp))(gen)) {
-            current_evaluated = next_evaluated;
-            if (best_score < next_score) {
-                best_score = next_score;
+        auto next = analyze_permutation(p, matrix,
+                max(0, current.ly - 5),
+                max(0, current.lx - 5),
+                min(s, current.ry + 5),
+                min(s, current.rx + 5) );
+        double delta = next.evaluated - current.evaluated;
+        if (current.evaluated < next.evaluated + eps or bernoulli_distribution(exp(delta / temp))(gen)) {
+            current.evaluated = next.evaluated;
+            if (best_score < next.score) {
+                best_score = next.score;
+                current = next;
                 result = p;
-visualize(p);
-cerr << "MESSAGE: iteration = " << iteration << endl;
-cerr << "MESSAGE: evaluated = " << int(current_evaluated) << endl;
-cerr << "MESSAGE: score     = " << int(best_score) << endl;
+// visualize(p);
+// cerr << "MESSAGE: iteration = " << iteration << endl;
+// cerr << "MESSAGE: evaluated = " << int(current.evaluated) << endl;
+// cerr << "MESSAGE: score     = " << int(best_score) << endl;
             }
         } else {
             swap(p[x], p[y]);
         }
     }
-cerr << "MESSAGE: score = " << best_score << endl;
+// cerr << "MESSAGE: score = " << best_score << endl;
     return result;
 }
 
