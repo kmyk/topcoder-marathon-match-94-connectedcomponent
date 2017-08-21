442daa11d602a28e75290ac72bf8c090a7fc59e6
Kimiyuki Onaka <kimiyuki95@gmail.com>
2017-08-22 01:32:00 +0900

    plot 勝敗比較もするように

--- plot.py
+++ plot.py
@@ -17,5 +17,16 @@ if __name__ == '__main__':
         df = pd.read_csv(path, delimiter='\t', index_col=0)
         df = df.rename(columns={ 'score': os.path.basename(path) })
         acc = pd.concat([ acc, df ], axis=1)
+    cnt = dict(map(lambda column: (column, 0), acc.columns))
+    for s in acc.index:
+        for column in acc.columns:
+            if acc.loc[s, column] == max(acc.loc[s]):
+                cnt[column] += 1.0 / list(acc.loc[s]).count(acc.loc[s, column])
+    for column in acc.columns:
+        cnt[column] /= len(acc.index)
+    cnt = list(cnt.items())
+    cnt.sort(key=lambda x: - x[1])
+    for column, value in cnt:
+        print column, value
     acc.plot(y=acc.columns, linestyle='-', marker='o')
     plt.show()
